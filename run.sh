3f08f47934336a09cb310d7659de36c57517e5b2
Antoine Arnoux <antoine.arnoux67@hotmail.fr>
2022-11-06 14:55:07 +0100

    adresse git

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 token=`cat token.txt`
-git pull -a "https://$token@github.com/FireITHole/CLOSEST-COMET-BOT.git"
+git pull -a "https://$token@github.com/FireITHole/CLOSESTCOMET-TWITTER.git"
 python3 NASA-TWITTER.py
 git add *
 CURRENTDATE=`date +"%Y-%m-%d/%T"`
 git commit -a -m "POST ${CURRENTDATE}"
-git push -u "https://$token@github.com/FireITHole/CLOSEST-COMET-BOT.git"
+git push -u "https://$token@github.com/FireITHole/CLOSESTCOMET-TWITTER.git"
